3b3f968be5735c2fc0314424c750b61371004d51
Jcbyro <jcbyro@gmail.com>
2024-01-18 15:27:42 -0300

    Django

--- Django/Practica/urls.py
+++ Django/Practica/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     # ("") un string que representa el URL
     # (views.index) Una funcion de views.py que se quiere llamar al visitar el URL
     # (name='index') Un nombre para el URL
-    path('brian', views.brian, name='brian'),
-    path('david', views.david, name='david'),
+    
+    #path('brian', views.brian, name='brian'),
+    #path('david', views.david, name='david'),
+    #Otra forma de poner estos 2 paths es con lo siguente
+    path('<str:name>', views.greet, name='greet'),
+    #Este nuevo path agarra cualquier nombre y te lo imprime como "Hola, nuevo_nombre"
 ]
--- Django/Practica/views.py
+++ Django/Practica/views.py
@@ -3,11 +3,24 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hola, Mundo")
+#def index(request):
+    #return HttpResponse("<h1 style=\"color:red\">Hola, Mundo</h1>")
                         #"Hola, Mundo" es lo que imprime al ponerle /Practica despues del URL
-def brian(request):
-    return HttpResponse("Hola, Brian")
 
-def david(request):
+def index(request):
+    return render(request, "Practica/index.html")
+
+#def brian(request):
+    return HttpResponse("Hola, Brian")
+    #Es mas oscuro por que representa que ya no sirve
+#def david(request):
     return HttpResponse("Hola, David")
+
+#def greet(request, name):
+    return HttpResponse(f"Hola, {name.capitalize()}")
+                                #.capitalize() hace que la primera palabra ahora este en mayuscula
+
+def greet(request, name):
+    return render(request, "Practica/greet.html", {
+        "name": name.capitalize()
+    })
